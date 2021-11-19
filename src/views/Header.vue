1ccb07928e20849d7c62965942f9c6808dba7692
Daniel <daniel.vellmar@gmail.com>
2021-11-19 01:33:09 +0100

    feat: header components, no functionality

---
+++ src/views/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <header class="d-flex flex-wrap justify-content-left border-bottom">
+        <h1>{{ title }}</h1>
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <input id="fileUpload" type="file" hidden>
+                <button @click="chooseFiles()" type="button" class="btn btn-light">
+                    <i class="fa fa-folder"></i>
+                    Import Folder
+                </button>
+            </li>
+            <li class="nav-item">
+                <button type="button" class="btn btn-light">
+                    <i class="fa fa-download"></i>
+                    Save File
+                </button>
+            </li>
+            <li class="nav-item">
+                <button type="button" class="btn btn-light">
+                    <i class="fa fa-file"></i>
+                    Tutorial
+                </button>
+            </li>
+        </ul>
+    </header>
+</template>
+
+
+<script>
+export default {
+    name: "Header",
+    props: {
+        title: String,
+    },
+    methods: {
+        chooseFiles() {
+            document.getElementById("fileUpload").click()
+        }
+    },
+}
+</script>
+
+<style scoped>
+header {
+    justify-content: space-between;
+    margin-bottom: 20px;
+}
+
+li {
+    margin: 5px;
+}
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,28 @@
 <template>
-  <graph v-if="showGraph" class="chart" :option="option" />
+    <div class="container-fluid">
+        <Header title="TF Annotator" />
+        <graph v-if="showGraph" class="chart" :option="option" />
+    </div>
 </template>
 
 <script>
 import graph from "./Graph.vue"
+import Header from "./Header.vue"
 
 export default {  
-  name: "Home",
-  components: {
-      graph,
-  },
-  data: () => {
-    return {
-      showGraph: true,
-    }
-  },
-  methods: {
-    
-  },
+    name: "Home",
+    components: {
+        graph,
+        Header,
+    },
+    data: () => {
+        return {
+        showGraph: true,
+        }
+    },
+    methods: {
+        
+    },
 };
 </script>
 
