56d1bfc6600ebf8a6054de071c3615cf923c5fa7
Daniel <daniel.vellmar@gmail.com>
2021-11-20 15:23:12 +0100

    feat: change Header to be responsive

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,4 +1,52 @@
 <template>
+    <div class="row justify-content-between">
+        <div id="col-header-title" class="col col-lg-auto col-md-auto col-sm-12 col-12">
+            <h1>{{ title }}</h1>
+        </div>
+        <div id="col-header-buttons" class="col col-lg-auto col-md-auto col-sm-12 col-12">
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <input id="fileUpload" type="file" hidden>
+                    <button @click="chooseFiles()" type="button" class="btn btn-light">
+                        <i class="fa fa-folder"></i>
+                        Import Folder
+                    </button>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="btn btn-light">
+                        <i class="fa fa-download"></i>
+                        Save File
+                    </button>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="btn btn-light">
+                        <i class="fa fa-file"></i>
+                        Tutorial
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+
+<script>
+/**
+<div class="col col-lg-2">
+            <input id="fileUpload" type="file" hidden>
+                <button @click="chooseFiles()" type="button" class="btn btn-light">
+                    <i class="fa fa-folder"></i>
+                    Import Folder
+                </button>
+        </div>
+        <div class="col col-lg-2">
+            Save
+        </div>
+        <div class="col col-lg-2">
+            Tutorial
+        </div>
+
+
     <header class="d-flex flex-wrap justify-content-left border-bottom">
         <h1>{{ title }}</h1>
         <ul class="nav nav-pills">
@@ -23,10 +71,7 @@
             </li>
         </ul>
     </header>
-</template>
-
-
-<script>
+*/
 export default {
     name: "Header",
     props: {
@@ -41,14 +86,17 @@ export default {
 </script>
 
 <style scoped>
-header {
-    justify-content: space-between;
+@media (max-width: 768px) {
+    ul {
+        justify-content: center;
+    }
+}
+
+.row {
     margin-bottom: 20px;
 }
 
 li {
     margin: 5px;
 }
-
-
 </style>
