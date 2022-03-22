ddbd10d388fdb4ee353474a9f731823dcbb54b50
Daniel <daniel.vellmar@gmail.com>
2022-03-22 14:06:22 +0100

    refactor: update fontawesome version via npm

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 createApp(App).use(store).use(router).mount('#app')
